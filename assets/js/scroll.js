121856f8fa5d8c18f476f6b300bdeda7a8c386fe
manoChampion <luisemanuel.alderete@gmail.com>
2018-04-23 04:23:13 -0300

    Css and Js fixes

---
+++ assets/js/scroll.js
@@ -0,0 +1,7 @@
+//Click Smooth Scroll
+$(document).on('click', 'a[href^="#"]', function(event){
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top}, 1000);
+});
